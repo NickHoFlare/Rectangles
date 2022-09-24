36ffb65691e7dad71878aa848bc5de76cbaffb6d
Nicholas Ho <terran324@gmail.com>
2022-09-24 10:03:31 +1000

    feat: Set up DI and initialize with Hello World

--- Rectangles/Program.cs
+++ Rectangles/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Microsoft.Extensions.DependencyInjection;
+using Rectangles;
+
+// Set up Dependency Injection
+var services = new ServiceCollection();
+ConfigureServices(services);
+
+var serviceProvider = services
+    .AddSingleton<IRectanglesService, RectanglesService>()
+    .BuildServiceProvider();
+
+// Program begins here
+var rectanglesService = serviceProvider.GetService<IRectanglesService>();
+if (rectanglesService != null)
+{
+    rectanglesService.Run();
+} 
+else
+{
+    Console.WriteLine("Something went wrong when initializing Rectangles");
+}
+
+/// <summary>
+/// Add auxiliary (non-core) dependencies to DI
+/// </summary>
+void ConfigureServices(IServiceCollection services)
+{
+
+}
